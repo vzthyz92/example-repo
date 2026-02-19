f0a22776e1da39ae2ba1182b956b643b8805bd30
Thys <vzthyz@gmail.com>
2026-02-19 14:47:52 +0200

    added new file details.py

---
+++ details.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ") #prompt user for their name
+age = input("Enter your age: ") #prompt user for their age
+house_num = input("Enter your house number: ") #prompt user for house number
+street_name = input("Enter your street name: ") # prompt user for street name
+print(f"This is {name}. He is {age} years old and lives at house number {house_num} on {street_name} ") #display inputs in a single string using the f-string method. Not including street in string to inlcude "road" or "avenue"
